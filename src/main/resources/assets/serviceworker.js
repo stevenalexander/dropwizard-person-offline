a6280602906e70c6b8836ab48f2ec6a155a9dce7
Steven Alexander <steven.william.alexander@googlemail.com>
2015-08-14 14:40:03 +0100

    added local copies of static resources and updated service worker to cache and return only specific responses

--- src/main/resources/assets/serviceworker.js
+++ src/main/resources/assets/serviceworker.js
@@ -1,15 +1,22 @@
 importScripts('/serviceworker-cache-polyfill.js');
 
+var CACHE_NAME = 'OFFLINE';
+var urlsToCache = [
+  './',
+  '/css/bootstrap/3.3.4/bootstrap.min.css',
+  '/js/libs/bootstrap/3.3.4/bootstrap.min.js',
+  '/js/libs/jquery/1.11.2/jquery.min.js',
+  '/js/main.js',
+  '/favicon.ico'
+];
+
 self.oninstall = function(event) {
   console.log('Installed!');
   self.skipWaiting();
 
   event.waitUntil(
-    caches.open("OFFLINE").then(function(cache) {
-      return cache.addAll([
-        './',
-        'main.js'
-      ]);
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
     })
   );
 };
@@ -18,13 +25,19 @@ self.onactivate = function(event) {
   console.log('Activated!');
 };
 
-//self.onfetch = function(event) {
-//  console.log('Fetching!: ' + event.request.url);
-//  var requestURL = new URL(event.request.url);
-//
-//  event.respondWith(
-//    caches.match(event.request, {
-//      ignoreVary: true
-//    })
-//  );
-//};
+self.onfetch = function(event) {
+  console.log('Fetching!: ' + event.request.url);
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          console.log('Fetched from cache!: ' + event.request.url);
+          return response;
+        }
+
+        console.log('Requesting!: ' + event.request.url);
+        return fetch(event.request);
+      }
+    )
+  );
+};
